584ebaae33cd017f7d217b18ee46dba07cc07b80
Guilherme S. Grittz <45982618+ggrittz@users.noreply.github.com>
2022-10-24 10:20:17 -0300

    Update 00_get_assessments.R

--- 00_get_assessments.R
+++ 00_get_assessments.R
@@ -16,15 +16,14 @@ spp = na.omit(spp)
 spp = spp[, 1]
 spp = as.data.frame(spp)
 
-#### Getting the IUCN assessments for Brazil (CNCFlora) - National level ####
+##### Getting the IUCN assessments for Brazil (CNCFlora) - National level #####
 tmp = flora::get.taxa(spp$spp, replace.synonyms = FALSE, life.form = TRUE)
 tmp$search.str = spp$spp
-tmp <- merge(spp, tmp, by.x = "spp", by.y = "search.str", all.x = TRUE, sort=FALSE)
+tmp <- merge(spp, tmp, by.x = "spp", by.y = "search.str", all.x = TRUE, sort = FALSE)
 #table(spp$spp == tmp$spp)
 spp$status.reflora <- tmp$threat.status
 
-
-#### Getting the global IUCN assessments (IUCN) ####
+##### Getting the global IUCN assessments (IUCN) ####
 #Citation: IUCN 2020. IUCN Red List of Threatened Species. Version 2020-1 <www.iucnredlist.org>
 iucn <- read.csv("IUCN_2020_assessments.csv", as.is = TRUE, na.strin = c(NA,""," "))
 
@@ -36,8 +35,7 @@ tmp <- merge(spp, iucn[, c("species.correct2" ,"redlistCategory", "redlistCriter
 tmp <- tmp[,c("spp", "status.reflora", "redlistCategory", "redlistCriteria")]
 table(spp$spp == tmp$spp)
 
-
-#### GET LOCAL ASSESSMENT FROM CONSEMA/2014 ####
+##### GET LOCAL ASSESSMENT FROM CONSEMA/2014 #####
 consema <- read.csv("consema_2014.csv", as.is = TRUE, header = TRUE, sep = ';')
 consema_spp <- consema$species
 consema_check <- flora::get.taxa(consema_spp, replace.synonyms = TRUE, life.form = TRUE)
@@ -50,7 +48,8 @@ consema[42, 2] <- "Heleocharis montevidensis"
 consema[98, 2] <- "Apoclada diversa"
 consema[129, 2] <- "Elaphoglossum jamesonii"
 
-final <- merge(tmp, consema[, c("species", "consema")], by.x = "spp", by.y = "species", all.x = TRUE, sort = FALSE)
+final <- merge(tmp, consema[, c("species", "consema")], 
+               by.x = "spp", by.y = "species", all.x = TRUE, sort = FALSE)
 final <- final[, c(1, 5, 2, 3, 4)]
 
 saveRDS(final, "rds_files/previous_assessments_spp.rds")
